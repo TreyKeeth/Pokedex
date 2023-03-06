be8c0e3f386ed7992a1b57512aa3dc14b47c221e
TreyKeeth <treykeeth@gmail.com>
2023-03-06 13:52:00 -0600

    Update array variable labeling
    Changed from let to const for the Pokemon array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemon = [
+const pokemon = [
     { name: 'Rattata', height: 0.3, types: ['normal'] }, 
     { name: 'Weedle', height: 0.3, types: ['bug', 'poison']},
     { name: 'Pikachu', height: 0.4, types: ['electric']},
