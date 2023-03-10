d9a2a2605fa4cfca0ef6e9bf196f1399d41f9f93
TreyKeeth <treykeeth@gmail.com>
2023-03-09 18:46:33 -0600

    Added object and loop to display list of pokemon
    Added Charizard, created a loop to display each pokemon on a separate line, and logic to call out that the pokemon is big when height is greater than 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
+
+// An initial list of pokemon with with each pokemon having a name, height, and an array of types since each can 
+// potentially have multiple types.
 const pokemon = [
     { name: 'Rattata', height: 0.3, types: ['normal'] }, 
+    { name: 'Charizard', height: 5.07, types: ['fire', 'flying'] },
     { name: 'Weedle', height: 0.3, types: ['bug', 'poison']},
-    { name: 'Pikachu', height: 0.4, types: ['electric']},
+    { name: 'Pikachu', height: 0.4, types: ['electric'] },
 ];
+
+// Loop through each of pokemon display their name, height, and calling out that a pokemon is big when the height is 
+// greater than 1.
+for (let i = 0; i < pokemon.length; i++) {
+    if (pokemon[i].height < 1) {
+        document.write(pokemon[i].name + ' ' + '(height) ' + pokemon[i].height + ' ' + "<br>")
+    }
+    if (pokemon[i].height >= 1) {
+        document.write(pokemon[i].name + ' ' + '(height) ' + pokemon[i].height + ' ' + 'Wow, that\'s big!' + "<br>")
+    }
+    
+    
+}
 
