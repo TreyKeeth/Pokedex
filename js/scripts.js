2f8ee63d355d556f0adcb57a780b81179f299003
TreyKeeth <treykeeth@gmail.com>
2023-03-17 18:15:06 -0500

    add forEach Pokemon array
    IIFE created to add Pokemon, print the output, and created get function for retrieval of the Pokemon array data.
    
    Called functions below the IIFE to verify successful add, get, and print functioning.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,48 @@
 
-// An initial list of pokemon with with each pokemon having a name, height, and an array of types since each can 
-// potentially have multiple types.
-const pokemon = [
-    { name: 'Rattata', height: 0.3, types: ['normal'] }, 
-    { name: 'Charizard', height: 5.07, types: ['fire', 'flying'] },
-    { name: 'Weedle', height: 0.3, types: ['bug', 'poison']},
-    { name: 'Pikachu', height: 0.4, types: ['electric'] },
-];
+// pokemonRepository variable for adding, retrieving, and printing the list of pokemons
+let pokemonRepository = (function () {
+    let pokemonList = []; 
 
-// Loop through each of pokemon display their name, height, and calling out that a pokemon is big when the height is 
-// greater than 1.
-for (let i = 0; i < pokemon.length; i++) {
-    if (pokemon[i].height < 1) {
-        document.write(pokemon[i].name + ' ' + '(height) ' + pokemon[i].height + ' ' + "<br>")
+// function within IIFE to add pokemon or print error option if an invalid name value is provided
+    function add(pokemon) {
+        if (typeof pokemon === "object" && "name" in pokemon) {
+            pokemonList.push(pokemon);
+        } else {
+            document.write("This is not a pokemon. Please enter a valid name." + "<br>");
+        }
     }
-    if (pokemon[i].height >= 1) {
-        document.write(pokemon[i].name + ' ' + '(height) ' + pokemon[i].height + ' ' + 'Wow, that\'s big!' + "<br>")
+// function to retrieve lists on pokemon in the array    
+    function getAll() {
+        return pokemonList;
     }
-    
-    
-}
+
+// function to print the list of pokemon in the array and also call out they are big if heigth is greater than 1    
+    function print() {
+        for (let i = 0; i < pokemonList.length; i++) {
+            if (pokemonList[i].height < 1) {
+                document.write(pokemonList[i].name + ' ' + '(height) ' + pokemonList[i].height + ' ' + "<br>")
+            }
+            if (pokemonList[i].height >= 1) {
+                document.write(pokemonList[i].name + ' ' + '(height) ' + pokemonList[i].height + ' ' + 'Wow, that\'s big!' + "<br>")
+            }
+        }
+    }
+    return {
+        add: add,
+        getAll: getAll,
+        print: print
+    };
+})();
+// adding pokemon to the array
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ['electric'] });
+pokemonRepository.add({ name: 'Rattata', height: 0.3, types: ['normal'] });
+pokemonRepository.add({ name: 'Charizard', height: 5.07, types: ['fire', 'flying'] });
+pokemonRepository.add({ name: 'Weedle', height: 0.3, types: ['bug', 'poison'] });
+// test to prompt the printing of the invalid Pokemon message in the add function within the IIFE
+pokemonRepository.add();
+// get of the pokemon list in the console
+console.log(pokemonRepository.getAll()); 
+// print the list of the pokemon
+pokemonRepository.print();
+
 
