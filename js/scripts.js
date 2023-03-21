d0bf7708fb50b8abf5857536c3e0434a098a025f
TreyKeeth <treykeeth@gmail.com>
2023-03-21 09:46:32 -0500

    refactor IIFE and print function
    Removed the print function from the IIFE and and made updates to call the print function using the getAll along with a forEach calling printPokemonList. Also included 2 of the pokemon from the array in the IIFE to verify both that pokemon populate both within the IIFE and when calling the add function outside of the IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 
 // pokemonRepository variable for adding, retrieving, and printing the list of pokemons
 let pokemonRepository = (function () {
-    let pokemonList = []; 
-
+    //initial list of pokemon
+    let pokemonList = [
+        { name: 'Pikachu', height: 0.4, types: ['electric'] },
+        { name: 'Rattata', height: 0.3, types: ['normal'] }
+    ];   
 // function within IIFE to add pokemon or print error option if an invalid name value is provided
     function add(pokemon) {
         if (typeof pokemon === "object" && "name" in pokemon) {
@@ -15,34 +18,30 @@ let pokemonRepository = (function () {
     function getAll() {
         return pokemonList;
     }
-
-// function to print the list of pokemon in the array and also call out they are big if heigth is greater than 1    
-    function print() {
-        for (let i = 0; i < pokemonList.length; i++) {
-            if (pokemonList[i].height < 1) {
-                document.write(pokemonList[i].name + ' ' + '(height) ' + pokemonList[i].height + ' ' + "<br>")
-            }
-            if (pokemonList[i].height >= 1) {
-                document.write(pokemonList[i].name + ' ' + '(height) ' + pokemonList[i].height + ' ' + 'Wow, that\'s big!' + "<br>")
-            }
-        }
-    }
     return {
         add: add,
         getAll: getAll,
-        print: print
     };
 })();
+
+//function to display list of Pokemon on interface
+function printPokemonList(pokemon) {
+    if (pokemon.height < 1) {
+                document.write(pokemon.name + ' ' + '(height) ' + pokemon.height + ' ' + "<br>")
+            } 
+            else {
+                document.write(pokemon.name + ' ' + '(height) ' + pokemon.height + ' ' + 'Wow, that\'s big!' + "<br>")
+            }
+};
+
 // adding pokemon to the array
-pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ['electric'] });
-pokemonRepository.add({ name: 'Rattata', height: 0.3, types: ['normal'] });
 pokemonRepository.add({ name: 'Charizard', height: 5.07, types: ['fire', 'flying'] });
 pokemonRepository.add({ name: 'Weedle', height: 0.3, types: ['bug', 'poison'] });
 // test to prompt the printing of the invalid Pokemon message in the add function within the IIFE
 pokemonRepository.add();
 // get of the pokemon list in the console
 console.log(pokemonRepository.getAll()); 
-// print the list of the pokemon
-pokemonRepository.print();
+// print the list of pokemon
+pokemonRepository.getAll().forEach(printPokemonList);
 
 
