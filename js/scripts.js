e3a083c209d6e1207388e8151295a2cf01c11da6
TreyKeeth <treykeeth@gmail.com>
2023-03-23 20:14:33 -0500

    Add Pokemon list buttons and re-factor IIFE
    Re-factored the IIFE criteria for checking if a pokemon addition is valid via the add function. addListItem function updated to add a button for each pokemon and to output information about a pokemon in the console via the showDetails function when a pokemon button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -2,36 +2,52 @@
 // pokemonRepository variable for adding, retrieving, and printing the list of pokemons
 let pokemonRepository = (function () {
     //initial list of pokemon
-    let pokemonList = [
+    let repository = [
         { name: 'Pikachu', height: 0.4, types: ['electric'] },
         { name: 'Rattata', height: 0.3, types: ['normal'] }
     ];   
 // function within IIFE to add pokemon or print error option if an invalid name value is provided
     function add(pokemon) {
-        if (typeof pokemon === "object" && "name" in pokemon) {
-            pokemonList.push(pokemon);
+        if (typeof pokemon === "object" &&
+            'name' in pokemon &&
+            'height' in pokemon && 
+            'types' in pokemon
+            ) {
+            repository.push(pokemon);
         } else {
             document.write("This is not a pokemon. Please enter a valid name." + "<br>");
         }
     }
 // function to retrieve lists on pokemon in the array    
     function getAll() {
-        return pokemonList;
+        return repository;
     }
+// function to print list of all pokemon and also show pokemon information in the console for a pokemon when their 
+// associated button is clicked
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listItem = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        })
+    }
+//function to show pokemon information in console
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
     return {
         add: add,
         getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
     };
 })();
-
-//function to display list of Pokemon on interface
-function printPokemonList(pokemon) {
-    if (pokemon.height < 1) {
-                document.write(pokemon.name + ' ' + '(height) ' + pokemon.height + ' ' + "<br>")
-        } else {
-                document.write(pokemon.name + ' ' + '(height) ' + pokemon.height + ' ' + 'Wow, that\'s big!' + "<br>")
-        }
-};
 
 // adding pokemon to the array
 pokemonRepository.add({ name: 'Charizard', height: 5.07, types: ['fire', 'flying'] });
@@ -41,6 +57,10 @@ pokemonRepository.add();
 // get of the pokemon list in the console
 console.log(pokemonRepository.getAll()); 
 // print the list of pokemon
-pokemonRepository.getAll().forEach(printPokemonList);
+// pokemonRepository.getAll().forEach(printPokemonList);
+// print the list of pokemon
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
 
 
