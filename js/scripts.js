62330652ef237abdb5851acc5f5ea8912e5486ca
TreyKeeth <treykeeth@gmail.com>
2023-04-10 14:29:29 -0500

    Add Modal to JavaScript
    Added a show modal function that displays when a pokemon is selected and added a hide modal function that closes the previously selected pokemon modal.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 
 // IIFE for adding, retrieving, and printing the list of pokemons
 let pokemonRepository = (function () {
-    let pokemonList = [];  
+    let pokemonList = [];
     // API where Pokemon information is pulled from
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         if (typeof pokemon === "object" &&
             'name' in pokemon
-            ) {
+        ) {
             pokemonList.push(pokemon);
         } else {
             document.write("This is not a pokemon. Please enter a valid name." + "<br>");
@@ -42,7 +42,6 @@ let pokemonRepository = (function () {
                     detailsUrl: item.url
                 };
                 add(pokemon);
-                console.log(pokemon);
             });
         }).catch(function (e) {
             console.error(e);
@@ -63,10 +62,72 @@ let pokemonRepository = (function () {
     }
 
     function showDetails(pokemon) {
-    pokemonRepository.loadDetails(pokemon).then(function () {
-        console.log(pokemon);
+        loadDetails(pokemon).then(function () {
+            showModal(pokemon);
         });
     }
+
+    let modalContainer = document.querySelector("#modal-container");
+
+    // display a modal once a pokemon is selected that displays the pokemon's name, height, types, image, and a close button
+    function showModal(item) {
+        pokemonRepository.loadDetails(item).then(function () {
+            let modal = document.createElement("div");
+            modal.classList.add("modal");
+
+
+            let pokemonName = document.querySelector(".modal-title");
+            pokemonName.innerText = item.name;
+
+            let pokemonImage = document.querySelector(".image");
+            pokemonImage.src = item.imageUrl;
+
+            let pokemonHeight = document.querySelector(".height");
+            pokemonHeight.innerHTML = "Height: " + item.height;
+
+            let allTypes = "";
+            item.types.forEach(function (types) {
+                allTypes += [types.type.name + "<br>"];
+            });
+
+            let pokemonTypes = document.querySelector(".types");
+            pokemonTypes.innerHTML = "Types: " + " " + allTypes + "<br>";
+            
+            let closeButtonElement = document.querySelector(".modal-close");
+            //closeButtonElement.classList.add(".modal-close");
+            closeButtonElement.innerText = "Close";
+            closeButtonElement.addEventListener('click', hideModal);
+
+
+            modal.appendChild(pokemonName);
+            modal.appendChild(pokemonImage);
+            modal.appendChild(pokemonHeight);
+            modal.appendChild(pokemonTypes)
+            modal.appendChild(closeButtonElement);
+            modalContainer.appendChild(modal);
+
+            modalContainer.classList.add("is-visible");
+        });
+    }
+
+    function hideModal() {
+        modalContainer.classList.remove("is-visible");
+    }
+
+    // close modal by pressing Escape/Esc key
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modalContainer.classList.contains("is-visible")) {
+            hideModal();
+        }
+    });
+
+    // close modal by clicking outside of the modal
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    });
 
     return {
         add: add,
@@ -75,8 +136,10 @@ let pokemonRepository = (function () {
         loadList: loadList, 
         loadDetails: loadDetails, 
         showDetails: showDetails, 
+        showModal: showModal,
     };
 })();
+
 
 pokemonRepository.loadList().then(function () {
     pokemonRepository.getAll().forEach(function (pokemon) {
